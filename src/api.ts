15c409d64e7d367d471a6fa8d140d41fc1574eea
amanusk <amanusk@kzencorp.com>
2020-01-29 11:22:09 +0200

    Add get all received transactions

--- src/api.ts
+++ src/api.ts
@@ -1,28 +1,29 @@
-const rp = require("request-promise");
+const rp = require('request-promise');
 const Chains = {
-  columbus_2: "https://fcd.terra.dev",
-  gaia: "http://ec2-54-189-179-159.us-west-2.compute.amazonaws.com:1317"
+  cosmoshub_3: 'https://fcd.terra.dev',
+  gaia: 'http://ec2-54-189-179-159.us-west-2.compute.amazonaws.com:1317',
 };
 
-export type ChainName = "gaia" | "columbus_2";
+export type ChainName = 'gaia' | 'cosmoshub_3';
 
 export async function get(chainName: ChainName, route: string): Promise<any> {
+  console.log(`${Chains[chainName]}${route}`);
   return rp({
-    method: "GET",
+    method: 'GET',
     uri: `${Chains[chainName]}${route}`,
-    json: true
+    json: true,
   });
 }
 
 export async function post(
   chainName: ChainName,
   route: string,
-  body: any
+  body: any,
 ): Promise<any> {
   return rp({
-    method: "POST",
+    method: 'POST',
     uri: `${Chains[chainName]}${route}`,
     body,
-    json: true
+    json: true,
   });
 }
