dd0a039272bbe45751b576dfebca604c39f33330
amanusk <amanusk@kznecorp.com>
2020-02-05 15:09:00 +0200

    Add staking unit test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,10 @@
-const { CosmosThreshSigClient, getBalance, mnemonicTransfer, getTxInfo, getTransactions } = require('../dist/src');
+const {
+    CosmosThreshSigClient,
+    getBalance,
+    mnemonicTransfer,
+    getTxInfo,
+    getTransactions,
+    getStakingInfo} = require('../dist/src');
 const assert = require('assert');
 
 const client = new CosmosThreshSigClient();
@@ -17,6 +23,8 @@ const account2 = {
   address: 'cosmos1qd59rlrm0ymz3r8lpj4yecazd38grxxjtw9zk6',
 };
 
+const validator = "cosmosvaloper169cxw3pkffw66vxlppy86n205rklhna8l8mtam";
+
 const UMUON_IN_MUON = 1000000;
 
 describe('Cosmos API tests', () => {
@@ -28,10 +36,10 @@ describe('Cosmos API tests', () => {
       const res = await client.transfer(bank.address, account1.address, "10000", "umuon");
       assert.ok(res.logs[0].success)
 
-      const balanceAfter= await getBalance(account1.address, network);
+      const newBalance= await getBalance(account1.address, network);
       const oldBalance = balanceBefore.result[0].amount || 0;
-      const newBalance = parseInt(oldBalance) + 10000
-      assert.deepEqual(balanceAfter.result[0], { denom: 'umuon', amount: `${newBalance}` });
+      const exptectedBalance = parseInt(oldBalance) + 10000
+      assert.strictEqual(exptectedBalance.toString(), newBalance.result[0].amount);
   }).timeout(100000);
 
   it('Transfers funds to an account then sends all funds back', async () => {
@@ -44,13 +52,30 @@ describe('Cosmos API tests', () => {
       assert.ok(res.logs[0].success)
       const balanceAfter= await getBalance(account2.address, network);
 
-      assert.deepEqual(balanceAfter.result[0], { denom: 'umuon', amount: '10000' });
+      assert.strictEqual(balanceAfter.result[0].amount, '10000');
 
       await client.transfer(account2.address, bank.address, "10000", "umuon", null, true);
 
       const balanceFinally = await getBalance(account2.address, network);
       assert.equal(balanceFinally.result.length, 0);
-
-
   }).timeout(100000);
+
+  it('Delegates funds to a validator', async () => {
+  const balanceBefore = await getStakingInfo(bank.address, network);
+
+  // Init the client
+  await client.init();
+  const res = await client.delegate(bank.address, validator, "10000", "umuon");
+  assert.ok(res.logs[0].success)
+
+  const newBalance= await getStakingInfo(bank.address, network);
+  const oldBalance = balanceBefore.result[0].balance || 0;
+  const exptectedBalance = parseInt(oldBalance) + 10000
+      console.log(oldBalance);
+      console.log(exptectedBalance);
+      console.log(newBalance);
+  assert.strictEqual(exptectedBalance.toString(), newBalance.result[0].balance);
+  }).timeout(100000);
+
+
 });
